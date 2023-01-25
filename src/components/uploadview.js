f290681a27aa978022dde2cf6d2d68f3f03d685f
Sanjaym-98 <122872856+Sanjaym-98@users.noreply.github.com>
2023-01-25 18:51:40 +0530

    Update uploadview.js

--- src/components/uploadview.js
+++ src/components/uploadview.js
@@ -13,7 +13,7 @@ export default function UploadView({ setIndicators }) {
         let dataFromForm = Object.fromEntries(formData.entries());
         console.log(dataFromForm);
 
-        await fetch("http://localhost:3004/posts", {
+        await fetch("https://msanjay.onrender.com/posts", {
             method: "post",
             body: formData,
             redirect: "follow"
