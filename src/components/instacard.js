fd66bb0e3fb5e39cfd857ce921ee3be5ee4f4e95
Sanjaym-98 <122872856+Sanjaym-98@users.noreply.github.com>
2023-02-06 00:06:10 +0530

    Update instacard.js

--- src/components/instacard.js
+++ src/components/instacard.js
@@ -4,16 +4,26 @@ import {SlPaperPlane} from "react-icons/sl";
 import "./card.css"
 
 
-export default function InstaCard({user}){
-    return <section className="card">
-        <h3 className="name"> {user.name}</h3>
-        <address className="location">{user.location}</address>
-        <BsThreeDots className="dots"/>
-        <img className="image" src={user.PostImage} alt="postimg"/>
-        <BsFillHeartFill className="heart"></BsFillHeartFill>
-        <SlPaperPlane className="plane"></SlPaperPlane>
-        <time className="data">{user.date}</time>
+export default function InstaCard({ user }) {
+    return<div className="container">
+        <div className="namecontainer">
+            <section className="card Section">
+                <h3 className="name"> {user.name}</h3>
+                <address className="location">{user.location}</address>
+            </section>
+            <section className="dots Section"><BsThreeDots /></section>
+        </div>
+        <section><img className="image" src={user.PostImage} alt="postimg" /></section>
+        <section className="favicons">
+            <div>
+              <i><BsFillHeartFill className="heart"></BsFillHeartFill></i>  
+               <i><SlPaperPlane className="plane"></SlPaperPlane></i> 
+            </div>
+            <time className="data">{user.date}</time>
+        </section>
         <sub className="likes">{user.likes}likes</sub>
         <div className="discription">{user.description}</div>
-    </section>
+
+    </div>
+    
 }
