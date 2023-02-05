53b68a3e649eb6b2e050e825d45d93d46320d916
Sanjaym-98 <122872856+Sanjaym-98@users.noreply.github.com>
2023-02-06 00:05:36 +0530

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -7,8 +7,10 @@ import "./header.css"
 export default function Header(){
     return (
         <div className="header">
-            <h1 className="topleft"><ImInstagram/></h1>
-            <h1 className="topright"><Link to = "/uploadview" ><BsFillCameraFill/></Link></h1>
+            
+            <h1 className="topleft icon"><ImInstagram/></h1>
+            <h1 className="icon">Instaclone</h1>
+            <h1 className="topright icon"><Link to = "/uploadview" ><BsFillCameraFill/>UploadPost</Link></h1>
         </div>
     )
 }
