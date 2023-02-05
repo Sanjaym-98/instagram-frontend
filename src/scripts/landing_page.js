c47c6efbd7b65596cfd2b4b6371371c6336d1c3a
Sanjaym-98 <122872856+Sanjaym-98@users.noreply.github.com>
2023-02-06 00:20:04 +0530

    Update landing_page.js

--- src/scripts/landing_page.js
+++ src/scripts/landing_page.js
@@ -4,15 +4,20 @@ import { Link } from 'react-router-dom';
  
 export default function Landing_page() {
   return(
-  <div class="container">
+  <div class="containers">
   {/* <h2>Landing Page</h2> */}
+  <div className='imagecontainer land'>
   <section class="left">
   <img class="landing_page_image" src="images/landingpage.jpg" alt="landing_pages"></img>
   </section>
-  <section class="right">
+  </div>
+  <div className='land button'>
+    <section class="right">
     <h1 class="text">10x Project</h1>
-    <Link to="/postview"><button> Enter </button></Link>
+    <Link to="/postview"><button className='btn'> Enter </button></Link>
   </section>
+  </div>
+  
   </div>
   )
 }
